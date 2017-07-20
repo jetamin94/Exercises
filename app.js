c2eacaca902041efc16b89facb4b543b4cc7c7c2
Jet <jetamin94@gmail.com>
2017-07-20 13:52:29 +0700

    update part3

---
+++ app.js
@@ -0,0 +1,193 @@
+// // Map và Set trong ES6
+// // Khởi tạo map
+// console.log('---------------MAP-----------------');
+// let map = new Map([
+//    ['name', 'Nguyen Van Thang'],
+//    ['email', 'jetamin94@gmail.com']
+// ]);
+
+// // Lặp qua các phần tử trong map
+// console.log('Show the keys and values of map');
+// for (let [keys, values] of map) {
+//    console.log(`${keys} - ${values}`);
+// }
+
+// console.log('--------------------------------');
+// // Thêm phần tử vào map
+// console.log('Add the item to map');
+// map.set('school', 'noname');
+// console.log(map);
+
+// console.log('--------------------------------');
+// console.log('Get an item on the map');
+// console.log(map.get('name'));
+
+// console.log('--------------------------------');
+// // Kiểm tra phần tử tồn tại
+// console.log('Check item on the map');
+// console.log(map.has('name')); // true
+// console.log(map.has('age')); // false
+
+// console.log('--------------------------------');
+// // Xóa phần tử trong map
+// console.log('Delete an item on the map');
+// map.delete('name');
+// console.log(map);
+
+// console.log('--------------------------------');
+// // Xóa toàn bộ phần tử trong map
+// console.log('Clear items on the map');
+// map.clear();
+// console.log(map);
+
+
+// console.log('----------------SET----------------');
+// // Khởi tạo set
+// let set = new Set([1, 2, 3, 4]);
+// console.log(set);
+
+// console.log('--------------------------------');
+// // Thêm phần tử vào set
+// console.log('Add an item to the Set');
+// set.add(6);
+// console.log(set);
+
+// console.log('--------------------------------');
+// // Kiểm tra phần tử có trong set
+// console.log('Check item from the Set');
+// console.log(set.has(3)); // true
+// console.log(set.has(9)); // false
+
+// console.log('--------------------------------');
+// // Xóa phần tử trong set
+// console.log('Delete item from the Set');
+// set.delete(2);
+// console.log(set);
+
+// console.log('--------------------------------');
+// // Chuyển đổi set sang mảng
+// console.log('Convert set to the array');
+// var arr = [...set];
+// console.log(arr);
+
+// console.log('--------------------------------');
+// // Chuyển đổi mảng sang set
+// console.log('Convert array to the map');
+// var newSet = new Set(arr);
+// console.log(newSet);
+
+// console.log('--------------------------------');
+// // Xóa toàn bộ các phần tử trong set
+// console.log('Delete all items on the Set');
+// set.clear();
+// console.log(set);
+
+// console.log('-------------WEAKMAP-------------');
+
+
+// // Weakmap giống như map nhưng không giống như key trong map có thể
+// // là rất nhiều loại, key trong weakmap phải là một object 
+// // (class, function, object)
+// // Weakmap cũng có những phương thức tương tự như map (thêm, xóa, 
+// // kiểm tra, lấy giá trị)
+
+// // Khởi tạo
+// var weak = new WeakMap();
+
+// // Danh sách keys
+// var key1 = {};
+// var key2 = {};
+
+// // Thêm giá trị cho weakmap
+// console.log('Set value for weakmap');
+// weak.set(key1, 'Value 1');
+// weak.set(key2, 'Value 2');
+// console.log(weak);
+
+// console.log('--------------------------------');
+// // Lấy giá trị trong weakmap
+// console.log('Get items from weakmap');
+// console.log(weak.get(key1));
+// console.log(weak.get(key2));
+
+// console.log('--------------------------------');
+// // Kiểm tra giá trị tồn tại trong map
+// console.log('Check item on weakmap');
+// var otherKey = {};
+// console.log(weak.has(key1)); // true
+// console.log(weak.has(otherKey)); // false
+
+// console.log('--------------------------------');
+// // Xóa phần tử trong weakmap
+// console.log('Delete an item on weakmap');
+// weak.delete(key1);
+// console.log(weak.get(key1));
+
+// console.log('-------------WEAKSET-------------');
+
+// // Weakset cũng tương tự như set nhưng thay vì bạn có
+// // thể tạo key rỗng để khởi tạo set thì bạn phải đặt 
+// // giá trị cho key khi khởi tạo weakset
+
+// // Weakset sẽ có một số thao tác chính như sau:
+// // + Khởi tạo
+// // + Thêm phần tử
+// // + Kiểm tra phần tử tồn tại
+// // + Xóa phần tử
+
+// // Khởi tạo
+// var weak = new WeakSet();
+
+// // Keys
+// var keySet1 = {
+//    name: 'Nguyen Van Thang'
+// };
+
+// var keySet2 = {
+//    website: 'jetamin94.com'
+// };
+
+// // Thêm phần tử
+// weak.add(keySet1);
+// weak.add(keySet2);
+
+// // Kiểm tra tồn tại
+// var otherKey = {};
+// console.log(weak.has(keySet1)); // true
+// console.log(weak.has(otherKey)); // false
+
+// // Xóa phần tử
+// weak.delete(keySet2);
+
+// 4.1
+const removeInvalid = (arr) => {
+
+}
+
+// 4.2
+const removeSpecific = (arr, num) => {
+
+}
+
+// 4.3
+const getRandom = (arr) => {
+
+}
+
+// 4.4
+const sumScores = (arr) => {
+
+}
+
+// 4.5
+const inscrease10 = (arr) => {
+
+}
+
+module.export = {
+    removeInvalid,
+    removeSpecific,
+    getRandom,
+    sumScores,
+    inscrease10
+}
--- test/test.js
+++ test/test.js
@@ -4,54 +4,40 @@ const should = chai.should()
 //const expect = chai.expect
 
 describe('Test part3', () => {
-	
-
-	// it('1.1 Hello function', () => {
-	// 	let res = server.hello()
-	// 	res.should.to.equal('Hello world!')
-	// })
-	// it('1.2 Square function', () => {
-	// 	let res = server.square(5)
-	// 	res.should.to.equal(25)
-	// })
-	// it('1.3 Above70 array', () => {
-	// 	let res = server.above70
-	// 	res.should.be.an('array');
-	// 	res.should.have.members([ 234, 74, 76, 92 ])
-	// })
-	// it('1.4 Filter then reduce', () => {
-	// 	let res = server.totalMachineValue
-	// 	res.should.to.equal(5727)
-	// })
-	// it('3.1 Enhanced Object Literals', () => {
-	// 	let res = server.price
-	// 	res.should.to.equal(40)
-	// })
-	// it('5.1, 5.2 Get name and age of person object', () => {
-	// 	let res = server.getInfo()
-	// 	res.should.be.an('object')
-	// 	res.should.have.property('name')
-	// 	res.should.have.property('age')
-	// 	res.name.should.equal('Joe')
-	// 	res.age.should.equal(21)
-	// })
-	// it('5.3 Get 2 first hobbies', () => {
-	// 	let res = server.getHobs()
-	// 	res.should.be.an('array')
-	// 	res[0].should.equal('football')
-	// 	res[1].should.equal('tennis')
-	// })
-	// it('5.4 Get person’s married.', () => {
-	// 	let res = server.getMarr()
-	// 	res.should.be.an('object')
-	// 	res.should.have.property('married')
-	// 	res.married.should.be.true
-	// })
-	// it('5.5 Get person’s phone and email.', () => {
-	// 	let res = server.getCont(request)
-	// 	res.should.be.an('object')
-	// 	res.should.have.property('phone')
-	// 	res.should.have.property('email')
-	// 	res.phone.should.equal('0123456789')
-	// })
+	it('4.1 Remove invalid data', () => {
+		let res = server.removeInvalid([NaN, 0, 16, false, -21, '', undefined, 45, null])
+		res.should.be.an('array')
+		res.should.have.members([16, -21, 45])
+	})
+	it('4.2 Remove a specific element', () => {
+		let res = server.removeSpecific([2, 5, 9, 6], 5)
+		res.should.be.an('array')
+		res.should.have.members([2, 9, 6])
+	})
+	it('4.3 Get a random item from an array', () => {
+		let res = serve.getRandom([1, 2, 3, 4, 5])
+		res.should.be.an('array')
+	})
+	it('4.4 Use reduce() to sum all scores', () => {
+		let res = serve.sumScores([
+			{
+				id: 1,
+				scores: [7, 8, 3, 4]
+			},
+			{
+				id: 2,
+				scores: [5, 10, 9, 6]
+			},
+			{
+				id: 3,
+				scores: [9, 7, 4, 8]
+			}
+		])
+		res.should.be.an('array')
+	})
+	it('4.5 Increase all elements by 10', () => {
+		let res = serve.inscrease10([5, 10, 15, 20, 25, 30])
+		res.should.be.an('array')
+		res.should.have.members([15, 20, 25, 30, 35, 40])
+	})
 })
